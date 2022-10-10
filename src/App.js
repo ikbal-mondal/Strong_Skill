b8ba065346051fe5c1f9c5e3da30e243fb461135
ikbal-mondal <kbalmondal062@gmail.com>
2022-10-10 21:08:25 +0530

    setup ready to works

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// logo import this 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h2>Hollo How are you ? </h2>
+    </div>
+  );
+}
+
+export default App;
